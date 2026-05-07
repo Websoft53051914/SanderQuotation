41c1051f733e0165788d87a2cfdc1fafb02d3edd
Alan <alan@websoft.com.tw>
2026-05-07 15:21:41 +0800

    feat:Ai歷史上資料上傳 畫面

---
+++ SanderQuotation/frontend/Controllers/HistoryFileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewModel;
+
+namespace frontend.Controllers
+{
+    public class HistoryFileController : BaseProjectController
+    {
+        private readonly IConfiguration _config;
+
+        public HistoryFileController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
